09a0adace88ad14aa6acd6042ed66de8cc5b3f6f
ZAKHAROV-Artem <szakharovartem@gmail.com>
2024-02-25 14:30:04 +0100

    Lesson 34. Handling all socket events on server

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import express, { Application } from "express";
 import cors from "cors";
 import "dotenv/config";
 import { Server } from "socket.io";
-import { ClientToServerEvents, ServerToClientEvents } from "./types";
+import { ClientToServerEvents, KeyValue, ServerToClientEvents } from "./types";
 
 const PORT = process.env.PORT || 8080;
 const app: Application = express();
@@ -22,6 +22,70 @@ const io = new Server<ClientToServerEvents, ServerToClientEvents>(
     },
   }
 );
+
+const meetings: KeyValue<{
+  ownerId: string;
+  ownerSocketId: string;
+  members: number;
+}> = {};
+
+io.on("connection", (socket) => {
+  socket.on("user:join-request", ({ code, user, ownerId }) => {
+    if (meetings[code]?.members >= 9) {
+      return socket.emit("meeting:full");
+    }
+    if (user.id === ownerId) {
+      if (!meetings[code]?.members) {
+        meetings[code] = {
+          ownerSocketId: socket.id,
+          ownerId: ownerId,
+          members: 1,
+        };
+      } else {
+        meetings[code].members = meetings[code].members + 1;
+      }
+      return socket.emit("user:accepted", { code, user });
+    }
+    if (!meetings[code]?.ownerId || !meetings[code]?.ownerSocketId) {
+      return socket.emit("user:wait-for-owner");
+    }
+    io.to(meetings[code].ownerSocketId).emit("user:join-request", {
+      ...user,
+      socketId: socket.id,
+    });
+  });
+
+  socket.on("user:accepted", ({ code, user }) => {
+    meetings[code].members = meetings[code].members + 1;
+    io.to(user.socketId).emit("user:accepted", { code, user });
+  });
+  socket.on("user:rejected", ({ code, user }) => {
+    io.to(user.socketId).emit("user:rejected", { code, user });
+  });
+  socket.on("meeting:join", ({ code, user }) => {
+    socket.join(code);
+    socket.to(code).emit("user:joined", user);
+    socket.on("user:toggle-audio", (userPeerId) => {
+      socket.to(code).emit("user:toggled-audio", userPeerId);
+    });
+
+    socket.on("user:toggle-video", (userPeerId) => {
+      socket.to(code).emit("user:toggled-video", userPeerId);
+    });
+    socket.on("disconnect", (data) => {
+      if (meetings[code]?.ownerSocketId === socket.id) {
+        meetings[code].ownerId = "";
+        meetings[code].ownerSocketId = "";
+      }
+      if (meetings[code]?.members <= 1) {
+        delete meetings[code];
+      } else {
+        meetings[code].members = meetings[code]?.members - 1 || 0;
+      }
+      socket.to(code).emit("user:left", user.peerId);
+    });
+  });
+});
 
 app.get("*", (req, res) => {
   res.redirect(process.env.CLIENT_URL || "http://localhost:3000");
